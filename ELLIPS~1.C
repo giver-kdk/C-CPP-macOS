f16f1076ffb382f450b35fe94213b605fc8b21ed
Revig <45360605+giver-kdk@users.noreply.github.com>
2022-11-13 19:41:24 +0545

    nm

--- ELLIPS~1.C
+++ ELLIPS~1.C
@@ -4,60 +4,58 @@
 
 int main()
 {
-    int x, y, rx, ry, xk, yk, pk;
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "C:\\TC\\bgi");
+	int x, y, rx, ry, xk, yk, pk;
+	int gd = DETECT, gm;
+	initgraph(&gd, &gm, "C:\\TC\\bgi");
 
-    printf("Enter x and y coordinates: ");
-    scanf("%d %d", &x, &y);
-    printf("Enter x-radius and y-radius of circle: ");
-    scanf("%d %d", &rx, &ry);
+	printf("Enter x and y coordinates: ");
+	scanf("%d %d", &x, &y);
+	printf("Enter x-radius and y-radius of circle: ");
+	scanf("%d %d", &rx, &ry);
 
-    xk = 0;
-    yk = ry;
-    pk = ry * ry + - (rx * rx) * ry + (rx * rx) / 4;
-    while((2 * xk * ry * ry) < (2 * yk * rx * rx))
-    {
-         if(pk < 0)
-        {
-
-            xk = xk + 1;
-            pk = pk + (2 * ry * ry * xk) + (ry * ry);
-        }
-        else if(pk >= 0)
-        {
-            xk = xk + 1;
-            yk = yk - 1;
-            pk = pk + (2 * ry * ry * xk) - (2 * rx * rx * yk) + (ry * ry);
-        }
-        putpixel(x + xk, y + yk, 4);
-	putpixel(x + yk, y + xk, 4);
-	putpixel(x - yk, y + xk, 4);
-	//putpixel(x - xk, y + yk, 4);
-	putpixel(x - xk, y - yk, 4);
-        //putpixel(x - yk, y - xk, 4);
-        //putpixel(x + yk, y - xk, 4);
-        //putpixel(x + xk, y - yk, 4);
-    }
-    pk = (ry * ry)*pow((xk + 0.5), 2) + (rx * rx)*pow((yk - 1), 2) - (pow(rx, 2) * pow(ry, 2));
-    while((2 * ry * 2 * xk) > (2 * rx * 2 * yk))
-    {
-	if(pk < 0)
+	xk = 0;
+	yk = ry;
+	pk = ry * ry + -(rx * rx) * ry + (rx * rx) / 4;
+	while ((2 * xk * ry * ry) < (2 * yk * rx * rx))
 	{
-		xk = xk + 1;
-		yk = yk - 1;
-		pk = pk + 2*(ry * ry)*xk - 2*(rx * rx)*yk + (rx * rx);
+		if (pk < 0)
+		{
+
+			xk = xk + 1;
+			pk = pk + (2 * ry * ry * xk) + (ry * ry);
+		}
+		else if (pk >= 0)
+		{
+			xk = xk + 1;
+			yk = yk - 1;
+			pk = pk + (2 * ry * ry * xk) - (2 * rx * rx * yk) + (ry * ry);
+		}
+		putpixel(x + xk, y + yk, 4);
+		putpixel(x + yk, y + xk, 4);
+		putpixel(x - yk, y + xk, 4);
+		// putpixel(x - xk, y + yk, 4);
+		putpixel(x - xk, y - yk, 4);
+		// putpixel(x - yk, y - xk, 4);
+		// putpixel(x + yk, y - xk, 4);
+		// putpixel(x + xk, y - yk, 4);
 	}
-	else if(pk >= 0)
+	pk = (ry * ry) * pow((xk + 0.5), 2) + (rx * rx) * pow((yk - 1), 2) - (pow(rx, 2) * pow(ry, 2));
+	while ((2 * ry * 2 * xk) > (2 * rx * 2 * yk))
 	{
-		yik = yk - 1;
-		pk = pk - 2*(rx * rx)*yk + (rx * rx);
+		if (pk < 0)
+		{
+			xk = xk + 1;
+			yk = yk - 1;
+			pk = pk + 2 * (ry * ry) * xk - 2 * (rx * rx) * yk + (rx * rx);
+		}
+		else if (pk >= 0)
+		{
+			yik = yk - 1;
+			pk = pk - 2 * (rx * rx) * yk + (rx * rx);
+		}
+		putpixel(x + xk, y + yk, WHITE);
 	}
-	putpixel(x + xk, y + yk, WHITE);
-
-    }
-   getch();
-   closegraph();
-   return 0;
-
+	getch();
+	closegraph();
+	return 0;
 }
