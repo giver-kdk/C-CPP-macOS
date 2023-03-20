5a3eeb4bb5d8690d313dc6eab9dc6341ab932af9
Revig <45360605+giver-kdk@users.noreply.github.com>
2023-03-20 17:45:52 +0545

    AI update

--- C Programming/TOC/MyCodes/DFA/test1.c
+++ C Programming/TOC/MyCodes/DFA/test1.c
@@ -1,35 +1,55 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+int state = -1;
+enum STATES{q0, q1};
+char inputs[2] = {'0', '1'};
+int initial = q0;
+int final[] = {q1};
+int n = 1;
+
+void dq0(char input)
+{
+	if(input == inputs[0]) state = q0;
+	else if(input == inputs[1]) state = q1;
+	else state = -1;
+}
+void dq1(char input)
+{
+	if(input == inputs[0]) state = q0;
+	else if(input == inputs[1]) state = q1;
+	else state = -1;
+}
+
 int main()
 {
-    char str[100];
-    int i, state=1;
-    printf("Enter any string of containing a and b\n ");
-    scanf("%s",str);
-    for(i=0;str[i]!='\0';i++)
-    {
-        switch(state)
-        {
-            case 0:
-            if(str[i]=='a')
-                state=1;
-            else if(str[i]=='b')
-                state=0;
-            break;
-            case 1:
-            if(str[i]=='a')
-                state=0;
-            else if(str[i]=='b')
-                state=1;
-            break;
-        }
-    }
-    if(state==1)
-    {
-        printf("\nstring is accepted as it contains even no. of a's");
-    }
-    else
-    {
-        printf("%s Rejected.\n", str);
-    }
+	int i;
+	char string[20], choice;
+	while(choice != 'n' && choice != 'N')
+	{
+		int isAccepted = 0;
+		fflush(stdin);
+		printf("Enter the string: ");
+		gets(string);
+		state = initial;
+		for(i = 0; string[i] != '\0'; i++)
+		{
+			if(state == q0) dq0(string[i]);
+			else if(state == q1) dq1(string[i]);
+			else break;
+		}
+		for(i = 0; i < n; i++)
+		{
+			if(state == final[i])
+			{
+				isAccepted = 1;
+				printf("\nString Accepted");
+				break;
+			}
+		}
+		if(isAccepted == 0) printf("\nString Rejected");
+		if(state == -1) printf("\nInvalid String");
+		printf("COntinue? (Y/N): ");
+		scanf(" %c", &choice);
+	}
+	return 0;
 }
