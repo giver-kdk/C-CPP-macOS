533db40e6384fb0c733d1f5f58aeceaeb60b19e1
Revig <45360605+giver-kdk@users.noreply.github.com>
2023-05-17 16:02:39 +0545

    Updated C-Cpp

--- C Programming/OS/test.cpp
+++ C Programming/OS/test.cpp
@@ -1,34 +1,22 @@
-#define N 100
-typedef int semaphore;
-semaphore empty = N;
-semaphore full = 0;
-semaphore mutex = 1;
+// #include <stdio.h>
+// #define N 100
+// typedef int semaphore;
+// semaphore empty = N;
+// semaphore full = 0;
+// semaphore mutex = 1;
 
 
-void producer()
-{
-	int item;
-	while(true)
-	{
-		item = produve_item();
 
-		down(&empty);
-		down(&mutex);
-		insert_item(item);
-		up(&mutex);
-		up(&full);
-	}
-}
-void producer()
-{
-	int item;
-	while(true)
-	{
-		down(&full);
-		down(&mutex);
-		item = remove_item();
-		up(&mutex);
-		up(&empty);
-		consume_item(item);
-	}
-}
+// void producer()
+// {
+// 	int item;
+// 	while(TRUE)
+// 	{
+// 		item = produce_item();
+// 		down(&empty);
+// 		down(&mutex);
+// 		insert_item(item);
+// 		up(&mutex);
+// 		up(&full);
+// 	}
+// }
